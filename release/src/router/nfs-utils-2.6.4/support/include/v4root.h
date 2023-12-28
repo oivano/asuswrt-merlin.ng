f1fb82d0bd4a9a64f76c925e53587f0de232cab8
ivano <23337370+oivano@users.noreply.github.com>
2023-12-28 21:42:31 +0100

    bumped to nfs-utils-2.6.4

---
+++ release/src/router/nfs-utils-2.6.4/support/include/v4root.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2009 Red Hat <nfs@redhat.com>
+ * support/include/v4root.h
+ *
+ * Support routines for dynamic pseudo roots.
+ *
+ */
+
+#ifndef V4ROOT_H
+#define V4ROOT_H
+
+extern int v4root_needed;
+extern void v4root_set(void);
+
+#endif /* V4ROOT_H */
+
+/*
+  * Should be, but isn't, defined in uuid/uuid.h
+*/
+#ifndef UUID_STR_LEN
+#define UUID_STR_LEN	36
+#endif
