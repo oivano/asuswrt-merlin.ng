44f26ba4250597665d1e5342beb90132ea1b11df
ivano <23337370+oivano@users.noreply.github.com>
2025-09-26 22:07:20 +0200

    modified:   release/src/router/shared/.gitignore

---
+++ release/src/router/shared/version.h
@@ -0,0 +1,14 @@
+#ifndef RTVERSION
+#define RT_MAJOR_VERSION "3.0"
+#define RT_MINOR_VERSION "0.4"
+#define RT_VERSION "3.0.0.4"
+#define RT_SERIALNO "386.14"
+#define RT_RCNO "3"
+#define RT_EXTENDNO "ion"
+#define RT_SWPJVERNO ""
+#define RT_BUILD_NAME "DSL-AC68U"
+#define RT_BUILD_NAME_SEC ""
+#define RT_BUILD_INFO "Fri Sep 26 21:50:09 UTC 2025 ion"
+#define RT_CUSTOM_MODEL ""
+#endif
+#define LIVE_UPDATE_RSA_VERSION ""
