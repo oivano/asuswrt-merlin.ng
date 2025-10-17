959f41e1aa5816a62fb9ed0751ad307bc20bdcfc
ivano <23337370+oivano@users.noreply.github.com>
2025-10-17 14:14:07 +0200

    frr: suppress version output

--- release/src/router/frr/lib/command.c
+++ release/src/router/frr/lib/command.c
@@ -259,7 +259,7 @@ void print_version(const char *progname)
 {
 	printf("%s version %s\n", progname, FRR_VERSION);
 	printf("%s\n", FRR_COPYRIGHT);
-	printf("configured with:\n\t%s\n", FRR_CONFIG_ARGS);
+//	printf("configured with:\n\t%s\n", FRR_CONFIG_ARGS);
 }
 
 char *argv_concat(struct cmd_token **argv, int argc, int shift)
@@ -1578,7 +1578,7 @@ DEFUN (show_version,
 	vty_out(vty, "%s %s (%s).\n", FRR_FULL_NAME, FRR_VERSION,
 		cmd_hostname_get() ? cmd_hostname_get() : "");
 	vty_out(vty, "%s%s\n", FRR_COPYRIGHT, GIT_INFO);
-	vty_out(vty, "configured with:\n    %s\n", FRR_CONFIG_ARGS);
+//	vty_out(vty, "configured with:\n    %s\n", FRR_CONFIG_ARGS);
 
 	return CMD_SUCCESS;
 }
