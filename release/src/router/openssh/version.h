b90f98f351ae75166269dfcfcdbbd52736c91876
ivano <23337370+oivano@users.noreply.github.com>
2026-07-31 20:27:29 +0200

    openssh: updated to openssh-sftp-server 10.4p1

--- release/src/router/openssh/version.h
+++ release/src/router/openssh/version.h
@@ -1,6 +1,6 @@
-/* $OpenBSD: version.h,v 1.105 2025/04/09 07:00:21 djm Exp $ */
+/* $OpenBSD: version.h,v 1.109 2026/07/06 07:54:26 djm Exp $ */
 
-#define SSH_VERSION	"OpenSSH_10.0"
+#define SSH_VERSION	"OpenSSH_10.4"
 
-#define SSH_PORTABLE	"p2"
+#define SSH_PORTABLE	"p1"
 #define SSH_RELEASE	SSH_VERSION SSH_PORTABLE
